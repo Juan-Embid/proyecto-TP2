c897bd3b824da5ceed556631ee000a0c8c6b8a43
zadowestaxd <alezombi@gmail.com>
2022-03-01 16:53:06 +0100

    Trabajando en el Junction
    Falta el advance del junction
    
    Co-Authored-By: Juan Embid <78643148+Juan-Embid@users.noreply.github.com>

--- src/simulator/model/Junction.java
+++ src/simulator/model/Junction.java
@@ -1,27 +1,47 @@
 package simulator.model;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class Junction extends SimulatedObject{
 	private List<Road> roadInList;
 	private Map<Junction, Road> roadOutList;
+	private Map<Road,List<Vehicle>> roadQueue;
 	private List<List<Vehicle>> queue;
-	private int greenLight, lastLightChange, x, y;
+	private int greenLight, lastLightSwitch, x, y;
 	private LightSwitchStrategy lightChangeStrategy;
 	private DequeuingStrategy extractElements;
 
 	Junction(String id, LightSwitchStrategy lsStrategy, DequeuingStrategy dqStrategy, int xCoor, int yCoor) {
-			//Observa que la constructora recibe las estrategias como parámetros. Debe comprobar que
-		//lsStrategy y dqStrategy no son null, y que xCoor y yCoor no son negativos, y lanzar una
-		//excepción en caso contrario
-		
 		super(id);
+		
+		if(lsStrategy == null || dqStrategy == null || xCoor < 0 || yCoor < 0)
+				throw new IllegalArgumentException("Error: invalid Argument");
+		
+		x = xCoor;
+		y = yCoor;
+		greenLight = -1;
+		lastLightSwitch = 0;
+		lightChangeStrategy = lsStrategy;
+		extractElements = dqStrategy;
+		roadOutList = new HashMap<>();
+		roadQueue = new HashMap<>();
+		queue = new ArrayList<>();
+		roadInList = new ArrayList<>();
+		
 	}
 	
 	@Override
 	void advance(int time) {
-		
+		if(greenLight != -1 && !queue.isEmpty()) {
+			
+		}
+			
 	}
 
 	@Override
@@ -30,24 +50,64 @@ public class Junction extends SimulatedObject{
 	}
 
 	void addIncommingRoad(Road r) {
+		if(r.destinyCross == this)
+			throw new IllegalArgumentException("Error: This road is invalid");
+		
+		roadInList.add(r);
+		queue.add(new ArrayList<>());
+		roadQueue.put(r, new ArrayList<>());
 		
 	}
 	
 	void addOutGoingRoad(Road r) {
 		
+		for (Road roads : roadInList) {
+			if(r.originCross != this || (roads != r && roads.originCross == this && roads.destinyCross == r.destinyCross))
+				throw new IllegalArgumentException("Error: invalids Roads");
+		}
+		roadOutList.put(r.destinyCross, r);
 	}
 	
 	void enter(Vehicle v) {
-		
+		roadQueue.get(v.getRoad()).add(v);	
 	}
 	
 	Road roadTo(Junction j) {
-		
-		return null;
+		return roadOutList.get(j);
 	}
 	
-	/*void advance(time t) {
+	@Override
+	public JSONObject report() {
+		JSONObject reportJSON = new JSONObject();
 		
-	}*/
+		reportJSON.put("id", _id);
+		if (greenLight == -1)
+			reportJSON.put("green",  "none");
+		else
+			reportJSON.put("green", roadInList.get(greenLight).getId());
+		reportJSON.put("queues", reportQueue());
+		return reportJSON;	
+	}
+	
+	private JSONObject reportRoad(Road road) {
+		JSONObject reportRoadJSON = new JSONObject();
+		JSONArray vehicles = new JSONArray();
+
+		reportRoadJSON.put("road", road.getId());
+		for (Vehicle vehicle : roadQueue.get(road)) 
+			vehicles.put(vehicle.toString());
+		reportRoadJSON.put("vehicles", vehicles);
+		
+		return reportRoadJSON;
+	}
+	
+	private JSONArray reportQueue() {
+		JSONArray reportQueueJSON = new JSONArray();
+		
+		for (Road road : roadInList) 
+			reportQueueJSON.put(reportRoad(road));
+			
+		return reportQueueJSON;
+	}
 	
 }
