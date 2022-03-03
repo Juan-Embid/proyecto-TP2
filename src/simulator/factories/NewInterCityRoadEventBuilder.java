92b4b0cdbffceb60997dd9c7421a215519c07839
zadowestaxd <alezombi@gmail.com>
2022-03-03 11:18:02 +0100

    NewIntercityEventBuilder Terminado
    Co-Authored-By: Juan Embid <78643148+Juan-Embid@users.noreply.github.com>

--- src/simulator/factories/NewInterCityRoadEventBuilder.java
+++ src/simulator/factories/NewInterCityRoadEventBuilder.java
@@ -3,11 +3,13 @@ package simulator.factories;
 import org.json.JSONObject;
 
 import simulator.model.Event;
+import simulator.model.NewInterCityRoadEvent;
+import simulator.model.Weather;
 
 public class NewInterCityRoadEventBuilder extends Builder<Event> {
 
-	NewInterCityRoadEventBuilder(String type) {
-		super(type);
+	NewInterCityRoadEventBuilder() {
+		super("new_inter_city_road");
 	}
 
 	@Override
@@ -20,20 +22,25 @@ public class NewInterCityRoadEventBuilder extends Builder<Event> {
 		int co2Limit = data.getInt("co2limit");
 		int maxSpeed = data.getInt("maxspeed");
 		String weather = data.getString("weather");
-		return null;
+		Weather weath = null;
+		 switch (weather) {
+		case "STORM":
+			weath = Weather.STORM;
+			break;
+		case "RAINY":
+			weath = Weather.RAINY;
+			break;
+		case "WINDY":
+			weath = Weather.WINDY;
+			break;
+		case "CLOUDY":
+			weath = Weather.CLOUDY;
+			break;
+		case "SUNNY":
+			weath = Weather.SUNNY;
+			break;
+		}
+		return new NewInterCityRoadEvent(time, id, src, dest, length, co2Limit, maxSpeed, weath);
 	}
 
 }
-/*{
-"type" : new_inter_city_road
-"data" : {
-	"time" : 1,
-	"id" : "r1",
-	"src" : "j1",
-	"dest" : "j2",
-	"length" : 10000,
-	"co2limit" : 500
-	"maxspeed" : 120,
-	"weather" : "SUNNY"
-}
-}*/
