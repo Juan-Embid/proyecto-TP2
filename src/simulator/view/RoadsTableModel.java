fc56c4668d313784f29cf114a027cd7a859eef92
zadowestaxd <alezombi@gmail.com>
2022-04-18 18:29:33 +0200

    Update RoadsTableModel.java
    Done

--- src/simulator/view/RoadsTableModel.java
+++ src/simulator/view/RoadsTableModel.java
@@ -1,71 +1,93 @@
 package simulator.view;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.table.AbstractTableModel;
 
 import simulator.control.Controller;
 import simulator.model.Event;
+import simulator.model.Road;
 import simulator.model.RoadMap;
 import simulator.model.TrafficSimObserver;
 
 public class RoadsTableModel extends AbstractTableModel implements TrafficSimObserver{
 
-	public RoadsTableModel(Controller __ctrl) {
-		// TODO Auto-generated constructor stub
+	private static final String[] columnNames = { "Id", "Length", "Weather", "Max. Speed", "Speed Limit", "Total CO2", "CO2 Limit" };
+
+	private List<Road> roads;
+	
+	public RoadsTableModel(Controller _ctrl) {
+		roads = new ArrayList<>();
+		_ctrl.addObserver(this);
 	}
 
 	@Override
 	public int getColumnCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return columnNames.length;
 	}
-
+	
+	public String getcolumnName(int i) {
+		return columnNames[i];
+	}
+	
 	@Override
 	public int getRowCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return roads.size();
 	}
 
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Object object = null;
+		switch(columnIndex) {
+		case 0:
+			object = roads.get(rowIndex).getId();
+		case 1:
+			object = roads.get(rowIndex).getLength();
+		case 2:
+			object = roads.get(rowIndex).getWeather();
+		case 3:
+			object = roads.get(rowIndex).getMaxSpeed();
+		case 4:
+			object = roads.get(rowIndex).getSpeedLimit();
+		case 5:
+			object = roads.get(rowIndex).getTotalCO2();
+		case 6:
+			object = roads.get(rowIndex).getContLimit();
+		}
+		return object;
 	}
 
 	@Override
 	public void onAdvanceStart(RoadMap map, List<Event> events, int time) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void onAdvanceEnd(RoadMap map, List<Event> events, int time) {
-		// TODO Auto-generated method stub
-		
+		roads = map.getRoads();
+		fireTableDataChanged();
 	}
 
 	@Override
 	public void onEventAdded(RoadMap map, List<Event> events, Event e, int time) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void onReset(RoadMap map, List<Event> events, int time) {
-		// TODO Auto-generated method stub
-		
+		roads = new ArrayList<>();
+		fireTableDataChanged();
 	}
 
 	@Override
 	public void onRegister(RoadMap map, List<Event> events, int time) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void onError(String err) {
-		// TODO Auto-generated method stub
 		
 	}
 
