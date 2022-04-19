2ffc4c1bf8af994206eed5eb5ae1f78dce76911c
zadowestaxd <alezombi@gmail.com>
2022-04-19 04:50:01 +0200

    ca

--- src/simulator/view/ChangeCO2ClassDialog.java
+++ src/simulator/view/ChangeCO2ClassDialog.java
@@ -122,6 +122,10 @@ public class ChangeCO2ClassDialog extends JDialog {
 		setVisible(false);
 	}
 	
+	public Integer getCO2Class() {
+		
+		return (Integer) co2Model.getSelectedItem();
+	}
 	public int open(RoadMap mapa) {
 		
 		for (Vehicle v : mapa.getVehicles())
@@ -139,152 +143,14 @@ public class ChangeCO2ClassDialog extends JDialog {
 		return estado;
 	}
 
-	public Integer getTicks() {
-		
-		return (Integer) ticks.getValue();
-	}
-
-	public Integer getCO2Class() {
-		
-		return (Integer) co2Model.getSelectedItem();
-	}
 	
 	public Vehicle getVehicle() {
 		
 		return (Vehicle) vehicleModel.getSelectedItem();
 	}
 
-}/*
-package simulator.view;
-
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Frame;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JSpinner;
-import javax.swing.SpinnerNumberModel;
-
-import simulator.misc.Pair;
-import simulator.model.Vehicle;
-
-
-public class ChangeCO2ClassDialog extends JDialog {
-
-	private static final long serialVersionUID = 1L;
-
-	private boolean status;
-	
-	private JComboBox<String> boxVehicles;
-	private JComboBox<Integer> boxCO2;
-	private JSpinner ticksSpinner;
-	private JButton cancelButton;
-	private JButton okButton;
-
-	ChangeCO2ClassDialog(Frame frame) {
-		super(frame, true);
-		initGui();
+public Integer getTicks() {
+		
+		return (Integer) ticks.getValue();
 	}
-	
-	private void initGui()  {
-		JPanel mainPanel = new JPanel();
-		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-		add(mainPanel);
-		
-		//Description;
-		JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 2, 3));
-		mainPanel.add(topPanel);
-		
-		JLabel descLabel = new JLabel("Schedule an event to change the CO2 class of a vehicle after a given number o simulation ticks from now.");
-		topPanel.add(descLabel);
-		
-		// Vehicles
-		JPanel centerPanel = new JPanel();
-		mainPanel.add(centerPanel);
-		
-		JLabel vehiclesLabel = new JLabel("Vehicle:");
-		centerPanel.add(vehiclesLabel);
-		
-		boxVehicles = new JComboBox<String>();
-		boxVehicles.setPreferredSize(new Dimension(50, 25));
-		centerPanel.add(boxVehicles);
-		
-		// CO2 
-		JLabel co2Label = new JLabel("CO2 Class:");
-		centerPanel.add(co2Label);
-		
-		boxCO2 = new JComboBox<Integer>();
-		boxCO2.setPreferredSize(new Dimension(50, 25));
-		for (int i = 0; i < 10; i++)
-			boxCO2.addItem(i);
-		centerPanel.add(boxCO2);
-		
-		// Ticks
-		JLabel ticksLabel = new JLabel("Ticks:");
-		centerPanel.add(ticksLabel);
-		ticksSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 1000, 1));
-		centerPanel.add(ticksSpinner);
-		
-		
-		//Cancel and Ok Buttons
-		JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 2, 3));
-		mainPanel.add(bottomPanel);
-		
-		cancelButton = new JButton("Cancel");
-		bottomPanel.add(cancelButton);
-		cancelButton.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				status = false;
-				setVisible(false);
-			}
-
-		});
-		
-		okButton = new JButton("Ok");
-		bottomPanel.add(okButton);
-		okButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if (boxVehicles.getSelectedItem() != null && boxCO2.getSelectedItem() != null)
-					status = true;
-				setVisible(false);
-			}
-		});
-		
-		pack();
-		setName("Change CO2 Class");
-		setLocationRelativeTo(null);
-	}
-	
-	boolean open(List<Vehicle> vehicles) {
-		
-		boxVehicles.removeAllItems();
-		for (Vehicle v : vehicles)
-			boxVehicles.addItem(v.toString());
-		
-		setVisible(true);
-		return status;
-	}
-	
-	List<Pair<String, Integer>> getNewCO2Vehicle() {
-		List<Pair<String, Integer>> data = new ArrayList<>();
-		String v = (String) boxVehicles.getSelectedItem().toString();
-		Integer co2 = (Integer) boxCO2.getSelectedItem();
-		data.add(new Pair<String, Integer>(v, co2));
-		return data;
-	}
-	
-	int getTime() { return (int) ticksSpinner.getValue(); }
-	
-}*/
+}
--- src/simulator/view/ChangeWeatherDialog.java
+++ src/simulator/view/ChangeWeatherDialog.java
@@ -1,146 +1,3 @@
-/*package simulator.view;
-
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.*;
-import simulator.model.*;
-
-public class ChangeWeatherDialog extends JDialog {
-	
-	private JPanel emergente;
-	private JLabel text;
-	private JLabel roadText;
-	private JComboBox<Road> road;
-	private JLabel textWeather;
-	private JComboBox<Weather> weather;
-	private JLabel textTicks;
-	private JSpinner ticks;
-		
-	private JPanel botones;
-	private JPanel opciones;
-	private JButton ok;
-	private JButton cancel;
-
-	private int estado = 0;
-	private DefaultComboBoxModel<Road> roadModel;
-	private DefaultComboBoxModel<Weather> weatherModel;
-
-	public ChangeWeatherDialog(Frame padre) {
-		
-		super(padre, true);
-		initGUI();
-	}
-
-	private void initGUI() {
-	
-		setTitle("Change Road Weather");
-			
-		emergente = new JPanel();
-		emergente.setLayout(new BoxLayout(emergente, BoxLayout.Y_AXIS));
-		setContentPane(emergente);
-			
-		text = new JLabel("<html>Schedule an event to change the weather class of a road after a given number of<br>simulation ticks form now.</html>");
-		text.setAlignmentX(CENTER_ALIGNMENT);
-		emergente.add(text);
-		emergente.add(Box.createRigidArea(new Dimension(0, 20)));		
-		
-		botones = new JPanel();
-		botones.setAlignmentX(CENTER_ALIGNMENT);
-		emergente.add(botones);
-			
-		roadText = new JLabel("Road: ", JLabel.CENTER);
-		roadModel = new DefaultComboBoxModel<Road>();
-		road = new JComboBox<Road>(roadModel);
-		road.setVisible(true);
-		botones.add(roadText);
-		botones.add(road);
-			
-		textWeather = new JLabel("CO2 Class: ", JLabel.CENTER);
-		weatherModel = new DefaultComboBoxModel<Weather>();
-		weather = new JComboBox<Weather>(weatherModel);
-		weather.setVisible(true);
-		botones.add(textWeather);
-		botones.add(weather);
-			
-		ticks = new JSpinner();
-		textTicks = new JLabel("Ticks: ", JLabel.CENTER);
-		ticks = new JSpinner(new SpinnerNumberModel(10, 1, 300, 1));
-		ticks.setMinimumSize(new Dimension(80, 30));
-		ticks.setMaximumSize(new Dimension(200, 30));
-		ticks.setPreferredSize(new Dimension(80, 30));
-			
-		botones.add(textTicks);
-		botones.add(ticks);
-			
-		opciones = new JPanel();
-		opciones.setAlignmentX(CENTER_ALIGNMENT);
-		emergente.add(opciones);
-		
-		cancel = new JButton("Cancel");
-		cancel.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				
-				estado = 0;
-				ChangeWeatherDialog.this.setVisible(false);	
-			}
-		});
-		opciones.add(cancel);
-
-		ok = new JButton("OK");
-		ok.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if ((weatherModel.getSelectedItem() != null) && (roadModel.getSelectedItem() != null))
-				{
-					estado = 1;
-					ChangeWeatherDialog.this.setVisible(false);
-				}
-			}
-		});
-		opciones.add(ok);
-
-		setPreferredSize(new Dimension(500, 200));
-		pack();
-		setResizable(false);
-		setVisible(false);
-	}
-		
-	public int open(RoadMap mapa) {
-		
-		for (Road r : mapa.getRoads())
-		{
-			roadModel.addElement(r);
-		}
-		for (Weather w : Weather.values())
-		{
-			weatherModel.addElement(w);
-		}
-		setLocation(getParent().getLocation().x + 10, getParent().getLocation().y + 10);
-		setVisible(true);
-			
-		return estado;
-	}
-
-	public Integer getTicks() {
-		
-		return (Integer) ticks.getValue();
-	}
-
-	public Weather getWeather() {
-			
-		return (Weather) weatherModel.getSelectedItem();
-	}
-		
-	public Road getRoad() {
-		
-		return (Road) roadModel.getSelectedItem();
-	}
-}*/
-
 package simulator.view;
 
 import java.awt.*;
@@ -152,27 +9,27 @@ import simulator.model.*;
 
 public class ChangeWeatherDialog extends JDialog {
 	
-	private JPanel emergente;
-	private JLabel texto;
-	private JLabel textoRoad;
+	private JPanel emerge;
+	private JLabel text;
+	private JLabel roadText;
+	private JLabel ticksText;
+	private JLabel weatherText;
 	private JComboBox<Road> road;
-	private JLabel textoWeather;
 	private JComboBox<Weather> weather;
-	private JLabel textoTicks;
 	private JSpinner ticks;
 		
-	private JPanel botones;
-	private JPanel opciones;
+	private JPanel buttons;
+	private JPanel options;
 	private JButton ok;
 	private JButton cancel;
 
-	private int estado = 0;
+	private int status = 0;
 	private DefaultComboBoxModel<Road> roadModel;
 	private DefaultComboBoxModel<Weather> weatherModel;
 
-	public ChangeWeatherDialog(Frame padre) {
+	public ChangeWeatherDialog(Frame father) {
 		
-		super(padre, true);
+		super(father, true);
 		initGUI();
 	}
 
@@ -180,57 +37,57 @@ public class ChangeWeatherDialog extends JDialog {
 	
 		setTitle("Change Road Weather");
 			
-		emergente = new JPanel();
-		emergente.setLayout(new BoxLayout(emergente, BoxLayout.Y_AXIS));
-		setContentPane(emergente);
+		emerge = new JPanel();
+		emerge.setLayout(new BoxLayout(emerge, BoxLayout.Y_AXIS));
+		setContentPane(emerge);
 			
-		texto = new JLabel("<html>Schedule an event to change the weather class of a road after a given number of<br>simulation ticks form now.</html>");
-		texto.setAlignmentX(CENTER_ALIGNMENT);
-		emergente.add(texto);
-		emergente.add(Box.createRigidArea(new Dimension(0, 20)));		
+		text = new JLabel("<html>Schedule an event to change the weather class of a road after a given number of<br>simulation ticks form now.</html>");
+		text.setAlignmentX(CENTER_ALIGNMENT);
+		emerge.add(text);
+		emerge.add(Box.createRigidArea(new Dimension(0, 20)));		
 		
-		botones = new JPanel();
-		botones.setAlignmentX(CENTER_ALIGNMENT);
-		emergente.add(botones);
+		buttons = new JPanel();
+		buttons.setAlignmentX(CENTER_ALIGNMENT);
+		emerge.add(buttons);
 			
-		textoRoad = new JLabel("Road: ", JLabel.CENTER);
+		roadText = new JLabel("Road: ", JLabel.CENTER);
 		roadModel = new DefaultComboBoxModel<Road>();
 		road = new JComboBox<Road>(roadModel);
 		road.setVisible(true);
-		botones.add(textoRoad);
-		botones.add(road);
+		buttons.add(roadText);
+		buttons.add(road);
 			
-		textoWeather = new JLabel("CO2 Class: ", JLabel.CENTER);
+		weatherText = new JLabel("CO2 Class: ", JLabel.CENTER);
 		weatherModel = new DefaultComboBoxModel<Weather>();
 		weather = new JComboBox<Weather>(weatherModel);
 		weather.setVisible(true);
-		botones.add(textoWeather);
-		botones.add(weather);
+		buttons.add(weatherText);
+		buttons.add(weather);
 			
 		ticks = new JSpinner();
-		textoTicks = new JLabel("Ticks: ", JLabel.CENTER);
+		ticksText = new JLabel("Ticks: ", JLabel.CENTER);
 		ticks = new JSpinner(new SpinnerNumberModel(10, 1, 300, 1));
-		ticks.setMinimumSize(new Dimension(80, 30));
 		ticks.setMaximumSize(new Dimension(200, 30));
+		ticks.setMinimumSize(new Dimension(80, 30));
 		ticks.setPreferredSize(new Dimension(80, 30));
 			
-		botones.add(textoTicks);
-		botones.add(ticks);
+		buttons.add(ticksText);
+		buttons.add(ticks);
 			
-		opciones = new JPanel();
-		opciones.setAlignmentX(CENTER_ALIGNMENT);
-		emergente.add(opciones);
+		options = new JPanel();
+		options.setAlignmentX(CENTER_ALIGNMENT);
+		emerge.add(options);
 		
 		cancel = new JButton("Cancel");
 		cancel.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				
-				estado = 0;
+				status = 0;
 				ChangeWeatherDialog.this.setVisible(false);	
 			}
 		});
-		opciones.add(cancel);
+		options.add(cancel);
 
 		ok = new JButton("OK");
 		ok.addActionListener(new ActionListener() {
@@ -239,33 +96,43 @@ public class ChangeWeatherDialog extends JDialog {
 			public void actionPerformed(ActionEvent e) {
 				if ((weatherModel.getSelectedItem() != null) && (roadModel.getSelectedItem() != null))
 				{
-					estado = 1;
+					status = 1;
 					ChangeWeatherDialog.this.setVisible(false);
 				}
 			}
 		});
-		opciones.add(ok);
+		options.add(ok);
 
 		setPreferredSize(new Dimension(500, 200));
 		pack();
 		setResizable(false);
 		setVisible(false);
 	}
+	
+	public Weather getWeather() {
 		
+		return (Weather) weatherModel.getSelectedItem();
+	}
+	
+public Road getRoad() {
+		
+		return (Road) roadModel.getSelectedItem();
+	}
+	
 	public int open(RoadMap mapa) {
 		
 		for (Road r : mapa.getRoads())
-		{
+		
 			roadModel.addElement(r);
-		}
+	
 		for (Weather w : Weather.values())
-		{
+		
 			weatherModel.addElement(w);
-		}
+		
 		setLocation(getParent().getLocation().x + 10, getParent().getLocation().y + 10);
 		setVisible(true);
 			
-		return estado;
+		return status;
 	}
 
 	public Integer getTicks() {
@@ -273,13 +140,6 @@ public class ChangeWeatherDialog extends JDialog {
 		return (Integer) ticks.getValue();
 	}
 
-	public Weather getWeather() {
-			
-		return (Weather) weatherModel.getSelectedItem();
-	}
 		
-	public Road getRoad() {
-		
-		return (Road) roadModel.getSelectedItem();
-	}
+	
 }
